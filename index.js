c302de033c3a1512a51d5a582b8b60cf01185b26
Avinash <avinash31803@gmail.com>
2023-07-08 12:58:56 +0530

    setup view engine

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port = 8000;
 // using express router
 app.use('/',require('./routes'));
 
+// view engine
+app.set('view engine','ejs');
+app.set('views','./views');
+
 app.listen(port,function(err){
     if(err){
         // console.log(err);
