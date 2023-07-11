a08840c11f6ebca2d1418aa90448b6b50fee99cf
Avinash <avinash31803@gmail.com>
2023-07-11 12:08:50 +0530

    added sign in functionality

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,7 +1,22 @@
 const User = require('../models/user');
 
 module.exports.profile = function (req, res) {
-    res.end("<h1>Profile</h1>");
+    // to check whether the user is authenticcated or not
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id)
+        .then((user)=>{
+            return res.render('userProfile',{
+                title: 'fizzFeed | Profile',
+                user : user,
+            })
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+    }else{
+        return res.redirect('/users/signIn');
+    }
+
 }
 
 // to render the sign in page
@@ -24,21 +39,6 @@ module.exports.create = function (req, res) {
         return res.redirect('back');
     }
 
-    // User.findOne({ email: req.body.email })
-    //     .then(user => {
-    //         if (user) {
-    //             return res.redirect('back');
-    //         }
-
-    //         return User.create(req.body);
-    //     })
-    //     .then(user => {
-    //         res.redirect('/users/signIn');
-    //     })
-    //     .catch(err => {
-    //         console.log("error in creating user in signup", err);
-    //         return res.redirect('back');
-    //     });
 
     User.findOne({ email: req.body.email })
         .then((user) => {
@@ -53,12 +53,35 @@ module.exports.create = function (req, res) {
         })
         .catch((err) => {
             console.log("Error:", err);
+            return res.redirect('back');
             // Handle the error appropriately
         });
 
 };
 
-
+// sign in and creating a session for the user
 module.exports.createSession = function (req, res) {
+    // finding the user in the db
+    User.findOne({email:req.body.email })
+    .then((user)=>{
+         // handle password which dont match
+        if(user.password != req.body.password){
+            console.log("Password incorrect");
+            return res.redirect('back');
+        }
+        // handle session creation
+        res.cookie('user_id',user.id);
+        return res.redirect('/users/userProfile');
+    })
+    .catch((err)=>{
+        console.error(`There was an issue with signing you in ${err}`)
+    })
 
+    // handling if user is found
+
+   
+
+    
+
+    // handling is the user is not found
 }
--- routes/users.js
+++ routes/users.js
@@ -4,10 +4,11 @@ const router = express.Router();
 
 const usersController = require('../controllers/user_controller');
  
-router.get('/profile',usersController.profile); 
+router.get('/userProfile',usersController.profile); 
 router.get('/signIn',usersController.signIn);
 router.get('/signUp',usersController.signUp);
 
 router.post('/create',usersController.create);
+router.post('/createSession',usersController.createSession);
 
 module.exports = router;
